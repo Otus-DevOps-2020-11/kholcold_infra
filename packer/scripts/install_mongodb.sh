58daf197b178c711ac578d80a6fd4b1574946896
Alexandr Kholodov <alexandr.khol@gmail.com>
2020-12-19 23:43:45 +0300

    Добавил основную часть

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt-get install -y apt-transport-https ca-certificates
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt-get update -y
+
+sleep 10
+
+apt-get install -y mongodb-org
+
+systemctl start mongod
+systemctl enable mongod
