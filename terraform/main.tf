4fefff495705f06af53adafde328635d403fc541
Alexandr Kholodov <alexandr.khol@gmail.com>
2021-01-12 01:13:53 +0300

    Добавил самостоятельные задания

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,14 @@
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
+
   name = "reddit-app"
+  zone = var.zone_inst
 
   resources {
     cores  = 2
@@ -31,16 +33,16 @@ resource "yandex_compute_instance" "app" {
   }
 
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
     agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/yc-user")
+    private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
